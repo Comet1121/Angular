17d916af82eb961f977de60834bb1a4c1e5e2b8b
Comet1121 <shwesin1171@gmail.com>
2019-10-31 00:51:53 +0630

    commit

--- Angular-test/src/app/app-routing.module.ts
+++ Angular-test/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentListComponent } from './views/student-list/student-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: StudentListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
